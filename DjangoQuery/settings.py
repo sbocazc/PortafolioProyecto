4dc8f0cbec11f804bb3a5d74a4c8b689071a218b
sbocazc <s.bocazc@gmail.com>
2020-03-19 13:56:41 -0300

    Cambios Web

--- DjangoQuery/settings.py
+++ DjangoQuery/settings.py
@@ -76,9 +76,11 @@ WSGI_APPLICATION = 'DjangoQuery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/XEPDB1',
+        'USER': '',
+        'PASSWORD': '',
+    },
 }
 
 
